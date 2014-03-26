d4eeb1bb79c5a361e234850d7667b1ba4fa442c5
Johannes Hofmeister <github@account.cessor.de>
2014-03-26 19:13:04 +0100

    Added setup.py and the license to the readme file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='substitute',
+    version='1.0',
+    description='A friendly substitute for python mocking frameworks',
+    author='Johannes Hofmeister',
+    author_email='substitute@spam.cessor.de',
+    url='http://cessor.github.io/substitute',
+    py_modules=['substitute'],
+    keywords=['mock','stub','fake','test','testing','tdd','bdd','substitute','substitution','replacement','unittesting']
+)
