755f01d55b435207deb5cc66d2d43607705564ad
Johannes Hofmeister <github@account.cessor.de>
2014-03-28 21:40:21 +0100

    Added moretests to verify mixed kwargs by type

--- test/test_verify_calls_with_mixed_arguments.py
+++ test/test_verify_calls_with_mixed_arguments.py
@@ -19,3 +19,20 @@ def test_expect_received_wrong_kwargs_mixed_fails():
 	component = Substitute()
 	component.method('Johannes', age=32)
 	expect_that(component.method.was_called_with('Johannes', age=12))
+
+def test_expect_received_mixed_data_by_type():
+	component = Substitute()
+	component.method('Johannes', age=32)
+	expect_that(component.method.was_called_with_any(str, age=int))
+
+@raises(WrongArgumentTypeComplaint)
+def test_expect_received_wrong_arg_mixed_by_type_fails():
+	component = Substitute()
+	component.method(5, age=32)
+	expect_that(component.method.was_called_with_any(str, age=int))
+
+@raises(WrongKeywordArgumentTypeComplaint)
+def test_expect_received_wrong_arg_mixed_by_type_fails():
+	component = Substitute()
+	component.method('hello', age=32)
+	expect_that(component.method.was_called_with_any(str, age=str))
