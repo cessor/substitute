13a147431234bd6ac3e89f54c8b14386225b4380
Johannes Hofmeister <github@account.cessor.de>
2014-03-28 19:41:57 +0100

    Moved files into a package folder, removed old files

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .substitute import *
-from .substitute import __all__ as substitute_all
-__all__ = substitute_all
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name='substitute',
-    version='1.0',
+    version='1.0.1',
     description='A friendly substitute for python mocking frameworks',
     author='Johannes Hofmeister',
     author_email='substitute@spam.cessor.de',
     url='http://cessor.github.io/substitute',
-    py_modules=['substitute'],
+    packages=['substitute'],
     keywords=['mock','stub','fake','test','testing','tdd','bdd','substitute','substitution','replacement','unittesting']
 )
