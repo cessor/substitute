70b74ae427a9ff93764f5a87b2e3476414daeabe
Johannes Hofmeister <github@account.cessor.de>
2014-03-28 21:43:20 +0100

    Don't mix tabs and spaces.

--- test/test_verify_calls_with_mixed_arguments.py
+++ test/test_verify_calls_with_mixed_arguments.py
@@ -5,40 +5,40 @@ expect_that = assert_true
 
 def test_expect_received_mixed_data():
     '''Substitute - Verify (mixed): Method was called with args and kwargs'''
-	component = Substitute()
-	component.method('Johannes', age=12)
-	expect_that(component.method.was_called_with('Johannes', age=12))
+    component = Substitute()
+    component.method('Johannes', age=12)
+    expect_that(component.method.was_called_with('Johannes', age=12))
 
 @raises(ArgumentMissmatchComplaint)
 def test_expect_received_wrong_arg_mixed_fails():
     '''Substitute - Verify (mixed): Method was called with wrong arg'''
-	component = Substitute()
-	component.method('John', age=12)
-	expect_that(component.method.was_called_with('Johannes', age=12))
+    component = Substitute()
+    component.method('John', age=12)
+    expect_that(component.method.was_called_with('Johannes', age=12))
 
 @raises(KeywordArgumentMissmatchComplaint)
 def test_expect_received_wrong_kwargs_mixed_fails():
     '''Substitute - Verify (mixed): Method was called with wrong kwarg'''
-	component = Substitute()
-	component.method('Johannes', age=32)
-	expect_that(component.method.was_called_with('Johannes', age=12))
+    component = Substitute()
+    component.method('Johannes', age=32)
+    expect_that(component.method.was_called_with('Johannes', age=12))
 
 def test_expect_received_mixed_data_by_type():
     '''Substitute - Verify (mixed): Method was called with args and kwargs (type)'''
-	component = Substitute()
-	component.method('Johannes', age=32)
-	expect_that(component.method.was_called_with_any(str, age=int))
+    component = Substitute()
+    component.method('Johannes', age=32)
+    expect_that(component.method.was_called_with_any(str, age=int))
 
 @raises(WrongArgumentTypeComplaint)
 def test_expect_received_wrong_arg_mixed_by_type_fails():
     '''Substitute - Verify (mixed): Method was called with wrong arg (type)'''
-	component = Substitute()
-	component.method(5, age=32)
-	expect_that(component.method.was_called_with_any(str, age=int))
+    component = Substitute()
+    component.method(5, age=32)
+    expect_that(component.method.was_called_with_any(str, age=int))
 
 @raises(WrongKeywordArgumentTypeComplaint)
 def test_expect_received_wrong_kwarg_mixed_by_type_fails():
-	'''Substitute - Verify (mixed): Method was called with wrong kwarg (type)'''
-	component = Substitute()
-	component.method('hello', age=32)
-	expect_that(component.method.was_called_with_any(str, age=str))
+    '''Substitute - Verify (mixed): Method was called with wrong kwarg (type)'''
+    component = Substitute()
+    component.method('hello', age=32)
+    expect_that(component.method.was_called_with_any(str, age=str))
